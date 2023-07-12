d20b6c423f22ad81fb03417202b500617793d3a3
Clayman <Clayman@DESKTOP-DM8E77U>
2023-07-12 17:15:53 +0300

    Delegation DONE

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -28,24 +28,16 @@ public:
 	//				Constructors:
 	explicit String(int size = 80) :size(size), str(new char[size] {})
 	{
-		//this->size = size;
-		//this->str = new char[size] {};
 		cout << "DefConstructor:\t" << this << endl;
 	}
-	String(const char* str) :size(strlen(str) + 1), str(new char[size] {})
+	String(const char* str) :String(strlen(str) + 1)
 	{
-		int a(2);
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
 		for (int i = 0; str[i]; i++)this->str[i] = str[i];
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other) :size(other.size), str(new char[size] {})
+	String(const String& other) :String(other.str)
 	{
 		//Deep copy
-		//this->size = other.size;
-		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
 	String(String&& other):size(other.size), str(other.str)
