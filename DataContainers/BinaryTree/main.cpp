8f7467754aab6227f5650971cbd604726fa4854a
Clayman <Clayman@DESKTOP-DM8E77U>
2023-09-02 02:47:12 +0300

    Binary tree day 2

--- DataContainers/BinaryTree/main.cpp
+++ DataContainers/BinaryTree/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Tree
 {
+protected:
 	class Element
 	{
 		int Data;
@@ -18,9 +19,15 @@ class Tree
 		{
 			cout << "EDestructor:\t" << this << endl;
 		}
+		friend class Tree;
+		friend class UniqueTree;
 	}*Root;
 public:
-	Tree():Root(nullptr)
+	Element* getRoot()
+	{
+		return Root;
+	}
+	Tree() :Root(nullptr)
 	{
 		cout << "TConstructor:\t" << this << endl;
 	}
@@ -29,7 +36,77 @@ public:
 		cout << "TDestructor:\t" << this << endl;
 	}
 
-	void insert()
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+
+	int Sum(Element* Root)
+	{
+		return Root == nullptr ? 0 : Sum(Root->pLeft) + Sum(Root->pRight) + Root->Data;
+	}
+	int Count(Element* Root)
+	{
+		return Root == nullptr ? 0 : Count(Root->pLeft) + Count(Root->pRight) + 1;
+	}
+	double Avg()
+	{
+		return (double)Sum(Root) / Count(Root);
+	}
+
+	int minValue(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+		/*if (Root->pLeft == nullptr)return Root->Data;
+		else return minValue(Root->pLeft);*/
+	}
+	int maxValue(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		return Root->pRight == nullptr ? Root->Data : maxValue(Root->pRight);
+		/*if (Root->pRight == nullptr)return Root->Data;
+		else return maxValue(Root->pRight);*/
+	}
+
+	void print(Element* Root)
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
+	}
+};
+
+class UniqueTree: public Tree
+{
+public:
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if(Data > Root->Data)
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
 };
 
 void main()
@@ -38,4 +115,32 @@ void main()
 	//caller - вызывающая функция
 	//callee - вызываемая функция
 	//Stack overflow exception
+	int n;
+	cout << "Введите размер дерева: "; cin >> n;
+	Tree tree;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100, tree.getRoot());
+	}
+	tree.print(tree.getRoot());
+	cout << endl;
+	cout << "Минимальное значение в дереве:  " << tree.minValue(tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
+	cout << "Сумма элементов дерева:\t\t" << tree.Sum(tree.getRoot()) << endl;
+	cout << "Количество элементов дерева:    " << tree.Count(tree.getRoot()) << endl;
+	cout << "Среднее-арифметическое элементов дерева: " << tree.Avg() << endl;
+
+
+	UniqueTree u_tree;
+	for (int i = 0; i < n; i++)
+	{
+		u_tree.insert(rand() % 100, u_tree.getRoot());
+	}
+	u_tree.print(u_tree.getRoot());
+	cout << endl;
+	cout << "Минимальное значение в дереве:  " << u_tree.minValue(u_tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << u_tree.maxValue(u_tree.getRoot()) << endl;
+	cout << "Сумма элементов дерева:\t\t" << u_tree.Sum(u_tree.getRoot()) << endl;
+	cout << "Количество элементов дерева:    " << u_tree.Count(u_tree.getRoot()) << endl;
+	cout << "Среднее-арифметическое элементов дерева: " << u_tree.Avg() << endl;
 }
---
+++ DataContainers/ListLibTest/lib/ReadMe PD_321.txt
@@ -0,0 +1,13 @@
+https://github.com/okovtun/PD_321.git
+https://www.youtube.com/watch?v=DJgLqTRrUiE&list=PLeqyOOqxeiINqPSzLEh9Oq9eIjtKj_rHv
+
+TODO:
+В Solution 'IntroductionToOOP' добавить проект 'Fraction', и в нем реализовать класс 'Fraction',
+описывающий простую дробь. В классе должны быть все необходимые методы и операторы;
+
+TODO:
+0. Выучить теорию!!!!!!!!!!!!!!!!!!!!!!!!!
+1. Написать метод ??? distance(???), который возвращает расстояние до указанной точки;
+2. Написать функцию ??? distance(???), которая возвращает расстояние между двумя точками;
+
+https://legacy.cplusplus.com/doc/tutorial/classes2/
